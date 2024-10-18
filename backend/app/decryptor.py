b819660f893d2cd7ae867ad43642d9207c1478bc
Gerardo <gerardo.solorzano@gmail.com>
2024-10-17 19:01:45 -0500

    decryptor OK

--- backend/app/decryptor.py
+++ backend/app/decryptor.py
@@ -6,10 +6,18 @@ from cryptography.hazmat.backends import default_backend
 from base64 import b64decode
 import hashlib
 import os
+from dotenv import load_dotenv
 
 app = FastAPI()
 
-SECRET_KEY = "$2b$12$/3x5iNUWe9/Hl7VCKmblcuYaK/wiQ4QITBuCLKpL93NHo1Xj8d9le"  # Replace with a strong secret key
+
+
+
+env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path=env_path)
+
+# Get variables from environment
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Function to derive AES key from the password
 def get_aes_key(password: str):
